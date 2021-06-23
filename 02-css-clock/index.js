aa989324b1be37913b90f1ff559219071aa420b8
Dave <mariobros1068@hotmail.com>
2021-06-23 00:47:35 -0500

    Se agrego las primeras funciones al reloj

--- 02-css-clock/index.js
+++ 02-css-clock/index.js
@@ -1,3 +1,14 @@
 
+const hora = document.querySelector('.hour-hand');
+const minuto = document.querySelector('.main-hand');
+const segundo = document.querySelector('.second-hand');
+
+const setDate = function(){
+    const tiempo = new Date();
+    const segundos = tiempo.getSeconds();
+}
+
+setInterval(setDate, 1000);
+
 
 
