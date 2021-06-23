031d930e98ab215a51fc2a27d26c4f2296760413
Dave <mariobros1068@hotmail.com>
2021-06-23 01:15:02 -0500

    Se termino de agregar la funcion al reloj

--- 02-css-clock/index.js
+++ 02-css-clock/index.js
@@ -6,6 +6,19 @@ const segundo = document.querySelector('.second-hand');
 const setDate = function(){
     const tiempo = new Date();
     const segundos = tiempo.getSeconds();
+    const segundosGrados = ((segundos / 60) * 360) + 90;
+    segundo.style.transform = `rotate(${segundosGrados}deg)`;
+
+    
+    
+    const minutos = tiempo.getMinutes();
+    const minutosGrados = ((minutos / 60) * 360) + 90;
+    minuto.style.transform = `rotate(${minutosGrados}deg)`;    
+
+    const horas = tiempo.getHours();
+   const horasGrados = ((horas / 12) * 360) + 90;
+   hora.style.transform = `rotate(${horasGrados}deg)`;    
+   
 }
 
 setInterval(setDate, 1000);
