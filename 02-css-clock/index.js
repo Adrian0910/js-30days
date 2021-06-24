eb9256fff9ac72a638d452456aa6a13650a77db7
Adrian0910 <uchihasantander@gmail.com>
2021-06-23 22:49:58 -0500

    spacing, blur and base color

--- 02-css-clock/index.js
+++ 02-css-clock/index.js
@@ -1,4 +1,28 @@
 
+/* 
+    ¿Como vamos a hacer eso?
+    - Crear una funcion que añada el estilo
+*/
 
+const hora = document.querySelector('.hour-hand');
+const minuto = document.querySelector('.min-hand');
+const segundo = document.querySelector('.second-hand');
 
-console.log('Hola. como estas')
+const setDate = function(){
+    const tiempo = new Date();
+    const segundos = tiempo.getSeconds();
+    const segundosGrados = ((segundos / 60) * 360) + 90;
+    segundo.style.transform = `rotate(${segundosGrados}deg)`;
+    console.log(segundosGrados)
+    
+    const minutos = tiempo.getMinutes();
+    const minutosGrados = ((minutos / 60) * 360) + 90;
+    minuto.style.transform = `rotate(${minutosGrados}deg)`;    
+
+    const horas = tiempo.getHours();
+    const horasGrados = ((horas / 12) * 360) + 90;
+    hora.style.transform = `rotate(${horasGrados}deg)`;    
+}
+
+setInterval(setDate, 1000);
+
