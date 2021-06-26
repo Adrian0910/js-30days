8db2416575001af1ce9ec7c5edb8fef91732fcaf
Adrian0910 <uchihasantander@gmail.com>
2021-06-25 23:31:57 -0500

    terminamos ejercicio 5

--- 05-flex-panels-image/index.js
+++ 05-flex-panels-image/index.js
@@ -1,5 +1,20 @@
 
+const panels = document.querySelectorAll(".panel");
 
 
 
+const imageOpen = function(){
+    console.log('hello')
+    this.classList.toggle('open');
+    // this.classList.toggle('open-active');
+}
+
+const imageTransform = function(e){
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+    }
+}
+
+panels.forEach(panel => panel.addEventListener('click', imageOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', imageTransform));
 
