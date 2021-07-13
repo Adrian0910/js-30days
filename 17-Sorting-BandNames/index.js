254429e5de8f8faf2a25ebeb80f4bc5291c0f9cb
Adrian0910 <uchihasantander@gmail.com>
2021-07-13 00:25:34 -0500

    Ejercicio 17: sorting

---
+++ 17-Sorting-BandNames/index.js
@@ -0,0 +1,38 @@
+const bands = [
+  'The Plot in You',
+  'The Devil Wears Prada',
+  'Pierce the Veil',
+  'Norma Jean',
+  'The Bled',
+  'Say Anything',
+  'The Midway State',
+  'We Came as Romans',
+  'Counterparts',
+  'Oh, Sleeper',
+  'A Skylit Drive',
+  'Anywhere But Here',
+  'An Old Dog',
+];
+
+
+
+const sortedBands = document.querySelector('#bands');
+
+
+const replace = function(a){
+    return a.replace(/^(a |the |an )/i,'').trim();
+}
+
+const orderSort = bands.sort( (a, b) => replace(a) > replace(b) ? 1 : -1);
+
+const orderEach = orderSort.forEach( (element) => {
+    sortedBands.innerHTML += `
+        <li>${element}</li>
+    `;    
+})
+
+
+
+
+
+
