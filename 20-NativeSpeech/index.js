4027c4af295d0534f6b104acbe239ada21e960aa
Dave <mariobros1068@hotmail.com>
2021-07-16 01:09:14 -0500

    Agregando el ejercicio numero 20: Se imprime lo que se dice mediante un microfono en la pantalla

---
+++ 20-NativeSpeech/index.js
@@ -0,0 +1,30 @@
+const speak = document.querySelector('.words');
+
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+
+let p = document.createElement('p');
+speak.appendChild(p);
+
+recognition.addEventListener('result', e => {
+    const transcript = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('')
+        p.textContent = transcript;
+        if (e.results[0].isFinal) {
+            p = document.createElement('p');
+            speak.appendChild(p);
+        }
+        if (transcript.includes('gato')) {
+            p.textContent = transcript + '🐈';
+            console.log('🐈');
+        }
+});
+
+recognition.addEventListener('end', recognition.start);
+
+recognition.start();
